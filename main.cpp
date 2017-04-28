bcb21811942f5bb6a057f6c6ec1982cac5c22efb
Luca Gasperini <git@xsoftware.tk>
2017-04-28 12:22:47 +0200

    XSoftware Password Manager 0.1
    -Create structure of the project
    
    Signed-off-by: Luca Gasperini <git@xsoftware.tk>

--- main.cpp
+++ main.cpp
@@ -1,32 +1,94 @@
 #include <xslib/xslib.h>
-#include <QFile>
-#include <QCryptographicHash>
-#include <QDir>
+#include <libxspasswd/xspasswd.h>
 #include <QDebug>
 
 using namespace std;
 
+QString strBuffer;
+QString strCmd;
+QStringList args;
+xsPasswd* lib = nullptr;
 
+QStringList params(const QString &str)
+{
+    QStringList offset = str.split(' ');
+    for(int i = 0; i < offset.size(); i++)
+        offset.value(i).trimmed();
+    return offset;
+}
+
+void create()
+{
+    QString hot1,hot2;
+    xsConsole() << "Enter new password -> ";
+    hot1 = xsConsole::ReadPasswd();
+    xsConsole() << "Repeat your password -> ";
+    hot2 = xsConsole::ReadPasswd();
+    if(hot1 != hot2)
+    {
+        xsConsole() << "Password mismatch!\n";
+        return;
+    }
+    hot2.clear();
+
+    if(lib->userCreate(hot1) == FAIL)
+    {
+        xsConsole() << lib->strStatus;
+    }
+    else
+    {
+        xsConsole() << "Creation successful on your home directory!\n";
+    }
+}
+
+void join()
+{
+    QString passwd;
+    do {
+    if(lib->iHit > lib->iMaxHit)
+        return;
+    xsConsole() << "Enter new password (" << QString::number(lib->iHit) << "/" << QString::number(lib->iMaxHit) << ") -> ";
+    passwd = xsConsole::ReadPasswd();
+    } while(lib->userJoin(passwd) == FAIL);
+    xsConsole() << "Welcome " << GETUSER << "\n";
+}
+
+void use(const QStringList &in)
+{
+    if(lib->tableUse(in.value(1)) == FAIL)
+        xsConsole() << "Impossible to find table " << in.value(1);
+    else
+        xsConsole() << "Using table " << in.value(1);
+}
+
+void add(const QStringList &in)
+{
+    QStringList out = in;
+    lib->dataAdd(out);
+}
+void get(const QStringList &in)
+{
+    lib->dataGet(in);
+}
 
 int main(int argc, char *argv[])
 {
-    while(buffer != "quit")
-    {
-        xsConsole() << status();
-        xsConsole() >> buffer;
+    lib = new xsPasswd();
+    join();
 
-        if(buffer.startsWith("create ",Qt::CaseInsensitive) && joined == 0)
-            if(create(buffer.split(" ").value(1)) == FAIL)
-                return 0;
-        if(buffer.startsWith("join ",Qt::CaseInsensitive) && joined == 0)
-            if(join(buffer.split(" ").value(1)) == FAIL)
-                return 0;
-        if(buffer.startsWith("use ",Qt::CaseInsensitive) && joined == 1)
-            use(buffer);
-        if(buffer.startsWith("add ",Qt::CaseInsensitive) && joined == 1)
-            add(buffer);
-        if(buffer.startsWith("get ",Qt::CaseInsensitive) && joined == 1)
-            get(buffer);
+    while(!strBuffer.startsWith("quit", Qt::CaseInsensitive))
+    {
+        xsConsole() << "[" << GETUSER << "]$ ";
+        xsConsole() >> strBuffer;
+        args = params(strBuffer);
+        strCmd = args.value(0);
+
+        if(strCmd.compare("use",Qt::CaseInsensitive) == 0)
+            use(args);
+        if(strCmd.compare("add",Qt::CaseInsensitive) == 0)
+            add(args);
+        if(strCmd.compare("get",Qt::CaseInsensitive) == 0)
+            get(args);
         //xsConsole() << usage();
 
     }
--- xspasswd.pro
+++ xspasswd.pro
@@ -22,3 +22,6 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+win32:CONFIG(release, debug|release): LIBS += -L/usr/lib/release/ -lxs -lxspasswd
+else:win32:CONFIG(debug, debug|release): LIBS += -L/usr/lib/debug/ -lxs -lxspasswd
+else:unix: LIBS += -L/usr/lib/ -lxs -lxspasswd
